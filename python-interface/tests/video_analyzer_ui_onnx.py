86cf7e78d635103b5dd6659dcb787c3c443d67c4
Jeffrey Queisser <jeffrey.queisser@oist.jp>
2024-04-14 22:42:02 +0900

    Added web-UI for video inference test... (opencv only compatible with f32 models, onnx is working with current model)

--- python-interface/tests/video_analyzer_ui_onnx.py
+++ python-interface/tests/video_analyzer_ui_onnx.py
@@ -10,13 +10,37 @@ from fastapi import Response
 
 from nicegui import Client, app, core, run, ui
 
+import beachbot
+
+vid_file = "/home/beachbot/Data/video_beach_test.mp4"
+model_file = "/home/beachbot/src/beach-cleaning-object-detection/Models/beachbot_yolov5s_beach-cleaning-object-detection__v2i__yolov5pytorch_1280/best.onnx"
+
+ai_detect = beachbot.ai.Yolo5Onnx(model_file=model_file)
+
 # In case you don't have a webcam, this will provide a black placeholder image.
 black_1px = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAA1JREFUGFdjYGBg+A8AAQQBAHAgZQsAAAAASUVORK5CYII='
 placeholder = Response(content=base64.b64decode(black_1px.encode('ascii')), media_type='image/png')
+
+
 # OpenCV is used to access the webcam.
-video_capture = cv2.VideoCapture(0)
+#video_capture = cv2.VideoCapture(0)
 
+video_capture = cv2.VideoCapture(vid_file)
+# # get total number of frames
+totalFrames = video_capture.get(cv2.CAP_PROP_FRAME_COUNT)
+# randomFrameNumber=random.randint(0, totalFrames)
+# # set frame position
+print("Open video")
+video_capture.set(cv2.CAP_PROP_POS_FRAMES,0)
+success, frame = video_capture.read()
+print("Load AI model")
+class_threshold=0.25
+confidence_threshold=0.2
+frame = ai_detect.crop_and_scale_image(frame)
+class_ids, confidences, boxes = ai_detect.apply_model_percent(frame, confidence_threshold=confidence_threshold, class_threshold=class_threshold)
+print("[ result is: ", [class_ids, confidences, boxes], "]")
 
+print("Prepare serer ...")
 
 
 def convert(frame: np.ndarray) -> bytes:
@@ -39,6 +63,43 @@ async def grab_video_frame() -> Response:
     return Response(content=jpeg, media_type='image/jpeg')
 
 
+def add_imgbox(pleft=0, ptop=0, w=0, h=0):
+    # color = 'SkyBlue'
+    color = '#FF0000' 
+    image.content += f'<rect x="{pleft*100}%" y="{ptop*100}%" ry="15" height="{h*100}%" width="{w*100}%" fill="none" stroke="{color}" stroke-width="4" />'
+    
+def rframe(fnum=0):
+    video_capture.set(cv2.CAP_PROP_POS_FRAMES,int(fnum))
+    succ, frame = video_capture.read()
+    frame = ai_detect.crop_and_scale_image(frame)
+    confidence_threshold = slider_th.value/1000.0
+    class_threshold = slider_clsth.value/1000.0
+    print("Detect with", confidence_threshold, "and", class_threshold)
+    class_ids, confidences, boxes = ai_detect.apply_model_percent(frame, confidence_threshold=confidence_threshold, class_threshold=class_threshold)
+    image.content = ""
+    for classid, confidence, box in zip(class_ids, confidences, boxes):
+        if confidence >= 0.01:
+            add_imgbox(*box)
+            
+
+    #print(obj_res)
+    return succ, frame
+
+@app.get('/file/frame')
+# Thanks to FastAPI's `app.get`` it is easy to create a web route which always provides the latest image from OpenCV.
+async def grab_file_frame(fnum=0) -> Response:
+    print("in")
+    if video_capture is None or not video_capture.isOpened():
+        return placeholder
+    # The `video_capture.read` call is a blocking function.
+    # So we run it in a separate thread (default executor) to avoid blocking the event loop.
+    _, frame = await run.io_bound(lambda : rframe(fnum))
+    if frame is None:
+        return placeholder
+    # `convert` is a CPU-intensive function, so we run it in a separate process to avoid blocking the event loop and GIL.
+    jpeg = await run.cpu_bound(convert, frame)
+    
+    return Response(content=jpeg, media_type='image/jpeg')
 
 
 async def disconnect() -> None:
@@ -113,18 +174,23 @@ with ui.page_sticky(position='bottom-right', x_offset=20, y_offset=20):
 
 
 with ui.row().classes('w-full'):
-    with ui.card().style('width: 45%'):
+    with ui.card().style('width: 90%'):
+        def up_img(obj : ui.interactive_image, pleft=0, ptop=0, w=25, h=25, val=0):
+            #color = 'SkyBlue'
+            #color = '#FF0000' 
+            #obj.content = f'<rect x="{pleft}%" y="{ptop}%" ry="15" height="{h}%" width="{w}%" fill="none" stroke="{color}" stroke-width="4" />'
+            #obj.set_source(f'/video/frame?{time.time()}')
+            obj.set_source(f'/file/frame?fnum={val}&t={time.time()}')
         ui.label('Video Analyzer:')
         image = ui.interactive_image(source="https://picsum.photos/id/684/640/360",size=(512,512)).style('width: 100%')
-        slider = ui.slider(min=0, max=100, value=50)
-        ui.label().bind_text_from(slider, 'value', backward=lambda a: f'Frame {a} of XX')
-        def up_img(obj : ui.interactive_image, pleft=0, ptop=0, w=25, h=25):
-            color = 'SkyBlue'
-            color = '#FF0000' 
-            obj.content = f'<rect x="{pleft}%" y="{ptop}%" ry="15" height="{h}%" width="{w}%" fill="none" stroke="{color}" stroke-width="4" />'
-            obj.set_source(f'/video/frame?{time.time()}')
+        slider = ui.slider(min=0, max=totalFrames, value=0, on_change=lambda x: up_img(image, val=x.value))
+        ui.label().bind_text_from(slider, 'value', backward=lambda a: f'Frame {a} of {totalFrames}')
+        slider_th = ui.slider(min=1, max=500, value=200, on_change=lambda x: up_img(image, val=slider.value))
+        ui.label().bind_text_from(slider_th, 'value', backward=lambda a: f'Confidence threshold is {a/1000.0}')
+        slider_clsth = ui.slider(min=1, max=500, value=250, on_change=lambda x: up_img(image, val=slider.value))
+        ui.label().bind_text_from(slider_clsth, 'value', backward=lambda a: f'Class threshold is {a/1000.0}')
 
-        ui.timer(1.0, lambda: up_img(image))
+        #ui.timer(1.0, lambda: up_img(image))
 
         
 

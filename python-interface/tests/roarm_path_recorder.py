8ccd16ef82b25bd940490caa4def0bce77991ea5
Jeffrey Queisser <jeffrey.queisser@oist.jp>
2024-05-24 14:13:41 +0900

    arm movement test files added

--- python-interface/tests/roarm_path_recorder.py
+++ python-interface/tests/roarm_path_recorder.py
@@ -4,7 +4,11 @@ import time
 arm1 = RoArmM1()
 arm1.refresh_robot_state()
 arm1.go_home()
+<<<<<<< HEAD
 qs, taus, ts = arm1.record_trajectory(wait_time_max=3, save_path="./pickup_path")
+=======
+qs, taus, ts = arm1.record_trajectory(wait_time_max=3, save_path="toss_path")
+>>>>>>> 544ae5a (arm movement test files added)
 print("Recoding entries:", qs.shape[0])
 print("Recoding duration:", ts[-1])
 # qs: joint angles
